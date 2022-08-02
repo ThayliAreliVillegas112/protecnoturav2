452f3e7a426fc07f10d8b367dbbb3c2b4a7c644e
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2022-08-02 14:01:23 -0500

    reventado

--- client/controller/reventadoController.js
+++ client/controller/reventadoController.js
@@ -5,16 +5,32 @@ const getReventadoById = async id => {
     }).done(res => res);
 };
 
+const getReventadoUsadoById = async id => {
+    return await $.ajax({
+        type: 'GET',
+        url: 'http://localhost:4000/reventado/usado2/' + id
+    }).done(res => res);
+};
+
 
 const getInfoReventado = async id => {
     var reventado = await getReventadoById(id);
     console.log(reventado);
   
-    // document.getElementById('matePrima').value = reventado.listReventado[0].name;
     document.getElementById('cantMPrima').value = reventado.listReventado[0].cantMateriaPrima;
     document.getElementById('cantAmaReve').value = reventado.listReventado[0].cantidadAmarantoRev;
     document.getElementById('fechaRe').value = reventado.listReventado[0].dateElaboracion;
     console.log(reventado);
+    console.log("si esta entrando");
+};
+
+const getInfoReventadoUsado = async id => {
+    var usado = await getReventadoUsadoById(id);
+    console.log(usado);
+  
+    document.getElementById('amarantoUsar').value = usado.listUsado[0].cantidadAmaranto;
+    // document.getElementById('dateRegistro').value = usado.listUsado[0].dateRegistro;
+    console.log(usado);
     console.log("si esta entrando");
 };
 
@@ -44,6 +60,32 @@ const getRegistrosAmaranto = () => {
 };
 getRegistrosAmaranto();
 
+const getRegistrosAmarantoUsado = () => {
+    $.ajax({
+        type: 'GET',
+        headers: { "Accept": "application/json" },
+        url: 'http://localhost:4000/reventado/usado'
+    }).done(res => {
+        console.log(res.listUsado);
+
+        let listUsado = res.listUsado;
+        let table = $("#tablaReventadoUsado");
+        
+        for (let i = 0; i < listUsado.length; i++) {
+            table.append(
+                "<tr>" +
+                "<td>" + (i+1) + "</td>" + 
+                "<td>" + listUsado[i].dateRegistro +"</td>" +
+                "<td>" + '<button onclick="getInfoReventadoUsado(' + listUsado[i].id + ');" type="button" class="btn btn-primary text-dark" data-bs-toggle="modal" data-bs-target="#detailsReventadoUsar"> <i class="fa fa-info infoBtn" aria-hidden="true"></i></button> </td>' +
+                "</tr>")
+                
+        }
+        
+    });
+};
+
+getRegistrosAmarantoUsado();
+
 function registerSegReventado (){
     event.preventDefault();
     const swalWithBootstrapButtons = Swal.mixin({
@@ -53,57 +95,25 @@ function registerSegReventado (){
         },
         buttonsStyling: false
     })
-    let cantMateriaPrima = document.getElementById('nameRe').value;
-    let cantidadAmarantoReve = document.getElementById('surnameRe').value;
-    let dateElaboracion = document.getElementById('lastnameRe').value;
-    var materiaPrima_id = document.getElementById('ageRe').value;
-    let address = document.getElementById('addressRe').value
-    let phone = document.getElementById('phoneRe').value;
-    let extension = document.getElementById('extensionRe').value;
-    let email = document.getElementById('emailRe').value;
-    let company = document.getElementById('companyRe').value;
-    let facebook = document.getElementById('faceRe').value;
-    let tiktok = document.getElementById('tiktokRe').value;
-    let instagram = document.getElementById('instagramRe').value;
-    // let photo = document.getElementById('photoRe').value;
-    // let photo1 = localStorage.getItem("photo");
-    // let image = photo1;
-    //     console.log("mmmmmmmmmmm");
-    //     console.log(photo1);
-
-if (name == "") {
+    var materiaPrima_id = 3;
+    let cantMateriaPrima = document.getElementById('cantM').value;
+    let cantidadAmarantoRev = document.getElementById('cantAmaReventado').value;
+    let dateElaboracion = document.getElementById('dateElaboracion').value;
+if (cantMateriaPrima == "") {
     Swal.fire({
-        title: "Completa el campo NOMBRE",
+        title: "Completa el campo CANTIDADA DE MATERIA PRIMA",
         confirmButtonText: "Aceptar",
         icon: "error",
     })
-} else if (surname == ""){
+} else if (cantidadAmarantoRev == ""){
     Swal.fire({
-        title: "Completa el campo APELLIDO MATERNO",
+        title: "Completa el campo CANTIDAD DE AMARANTO REVENTADO",
         confirmButtonText: "Aceptar",
         icon: "error",
     })
-}else if(lastname == ""){
+}else if(dateElaboracion == ""){
     Swal.fire({
-        title: "Completa el campo APELLIDO PATERNO",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(address == ""){
-    Swal.fire({
-        title: "Completa el campo DIRECCIÓN",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(phone == ""){
-    Swal.fire({
-        title: "Completa el campo TELÉFONO",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(email == ""){
-    Swal.fire({
-        title: "Completa el campo CORREO",
+        title: "Completa el campo FECHA DE ELABORACIÓN",
         confirmButtonText: "Aceptar",
         icon: "error",
     })
@@ -120,18 +130,22 @@ if (name == "") {
         if(result.isConfirmed){
             $.ajax({
                 type: 'POST',
-                url: 'http://localhost:4000/client/create',
-                data: { name, surname, lastname, age, address, phone, extension, email, company, facebook, tiktok, instagram}
+                url: 'http://localhost:4000/reventado/create',
+                data: { cantMateriaPrima, cantidadAmarantoRev, dateElaboracion, materiaPrima_id}
             }).done(res => {
                 console.log(res)
                 if (res.status === 200) {
                     swalWithBootstrapButtons.fire(
                         'Registro exitoso',
-                        'Se ha registrado al cliente exitosamente',
+                        'Se ha registrado el seguimiento de reventado',
                         'success'
                     )
-                    let formulario = document.getElementById('formu'); 
-                    formulario.reset()
+                    $('#reventadoRegister'). modal('hide');  //Sirve para cerrar el modal despues de aceptar la eliminación
+                    setTimeout(function() {
+                        let refresh = document.getElementById('tablaReventado');
+                         refresh= location.reload();
+                        location.reload(true);
+                    }, 3000);
                 } else {
                     Swal.fire({
                         title: "Hubo un problema al registrar",
@@ -150,7 +164,8 @@ if (name == "") {
     })
 }};
 
-function updateClient (){
+
+function registerSegReventadoUsar (){
     event.preventDefault();
     const swalWithBootstrapButtons = Swal.mixin({
         customClass: {
@@ -159,64 +174,23 @@ function updateClient (){
         },
         buttonsStyling: false
     })
-    console.log(id);
-    console.log("Si entra para hacer los cambios");
-    var id = document.getElementById('id_updateC').value;
-    let name = document.getElementById('name_up').value;
-    let surname = document.getElementById('surname_up').value;
-    let lastname = document.getElementById('lastname_up').value;
-    let age = document.getElementById('age_up').value;
-    let address = document.getElementById('address_up').value;
-    let phone = document.getElementById('phone_up').value;
-    let extension = document.getElementById('extension_up').value;
-    let email = document.getElementById('email_up').value;
-    let company = document.getElementById('company_up').value;
-    let facebook = document.getElementById('face_up').value;
-    let tiktok = document.getElementById('tiktok_up').value;
-    let instagram = document.getElementById('instagram_up').value;
-    // let photo = document.getElementById('photo_up').value;
-
-    console.log(id);
-
-if (name == "") {
+    let cantidadAmaranto = document.getElementById('amarantoUsar2').value;
+    let dateRegistro = document.getElementById('date').value;
+if (cantidadAmaranto == ""){
     Swal.fire({
-        title: "Completa el campo NOMBRE",
+        title: "Completa el campo CANTIDAD DE AMARANTO",
         confirmButtonText: "Aceptar",
         icon: "error",
     })
-} else if (surname == ""){
+}else if(dateRegistro == ""){
     Swal.fire({
-        title: "Completa el campo APELLIDO MATERNO",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(lastname == ""){
-    Swal.fire({
-        title: "Completa el campo APELLIDO PATERNO",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(address == ""){
-    Swal.fire({
-        title: "Completa el campo DIRECCIÓN",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(phone == ""){
-    Swal.fire({
-        title: "Completa el campo TELÉFONO",
-        confirmButtonText: "Aceptar",
-        icon: "error",
-    })
-}else if(email == ""){
-    Swal.fire({
-        title: "Completa el campo CORREO",
+        title: "Completa el campo FECHA",
         confirmButtonText: "Aceptar",
         icon: "error",
     })
 }else{
     swalWithBootstrapButtons.fire({
-        title: 'Estás seguro de realizar los cambios?',
+        title: 'Estás seguro de realizar el registro?',
         text: "Te sugerimos que revises la información antes de registrar",
         icon: 'warning',
         showCancelButton: true,
@@ -227,25 +201,25 @@ if (name == "") {
         if(result.isConfirmed){
             $.ajax({
                 type: 'POST',
-                url: 'http://localhost:4000/client/update/' + id,
-                data: { name, surname, lastname, age, address, phone, extension, email, company, facebook, tiktok, instagram }
+                url: 'http://localhost:4000/reventado/createUsado',
+                data: { cantidadAmaranto, dateRegistro}
             }).done(res => {
                 console.log(res)
                 if (res.status === 200) {
                     swalWithBootstrapButtons.fire(
-                        'Modificación exitosa',
-                        'Se ha modificado al cliente exitosamente',
+                        'Registro exitoso',
+                        'Se ha registrado el seguimiento de salida de amaranto reventado',
                         'success'
                     )
-                    $('#update'). modal('hide');  //Sirve para cerrar el modal despues de aceptar la eliminación
+                    $('#dismiurStock2'). modal('hide');  //Sirve para cerrar el modal despues de aceptar la eliminación
                     setTimeout(function() {
-                        let refresh = document.getElementById('tabla');
-                        refresh= location.reload();
-                         location.reload(true);
-                    }, 2000);
+                        let refresh = document.getElementById('tablaReventadoUsado');
+                         refresh= location.reload();
+                        location.reload(true);
+                    }, 3000);
                 } else {
                     Swal.fire({
-                        title: "Hubo un problema al modificar",
+                        title: "Hubo un problema al registrar",
                         confirmButtonText: "Aceptar",
                         icon: "error",
                     });
@@ -254,7 +228,7 @@ if (name == "") {
         }{
             swalWithBootstrapButtons.fire(
                 'Acción cancelada',
-                'No se ha realizado la modificación',
+                'No se ha realizado el registro',
                 'error'
             )
         }
@@ -262,9 +236,11 @@ if (name == "") {
 }};
 
 
+
+
 function doSearch()
     {
-        const tableReg = document.getElementById('tabla');
+        const tableReg = document.getElementById('tablaReventado');
         const searchText = document.getElementById('search-focus').value.toLowerCase();
         let total = 0;
 
@@ -299,66 +275,3 @@ function doSearch()
 
         }
 }
-
-
-
-// VALIDACIÓN DE FORMULARIO
-
-function soloLetras(e) {
- 
-    var key = e.keyCode || e.which,
-    tecla = String.fromCharCode(key).toLowerCase(),
-    letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙ",
-    especiales = [8, 37, 39, 46],
-    tecla_especial = false;
- 
-    for (var i in especiales) {
-        if (key == especiales[i]) {
-            tecla_especial = true;
-            break;
-            }
-        }
- 
-    if (letras.indexOf(tecla) == -1 && !tecla_especial) {
-        return false;
-    }
-}
- 
-// // -- Función para aceptar espacios -- //
-// function valeft(){
- 
-//     var val = document.getElementById("nombreLeft").value;
-//     var tam = val.length;
- 
-//         for(i=0;i<tam;i++){
-//             if(!isNaN(val[i]) && val[i] != " ")
-//             document.getElementById("nombreLeft").value='';
-//             }
-// }
-
-// VALIDACIÓN SOLO NUMEROS
-
-
-function esNum(ev, el) {
-    /* ev: evento; el: elemento; */
-    tecla = (document.all) ? ev.keyCode : ev.which;
-
-    // Permite un solo punto decimal
-    if (el.value.indexOf('.') == -1 ? tecla == 46 : false) { return true; }
-
-    // Permite números negativos (después de escribir un número)
-    if (tecla == 45 && el.value !== '') { el.value = (-1) * el.value; }
-
-    // Permite tecla de retroceso (borrar)
-    if (tecla == 8) { return true; }
-
-    // Permite tecla Tab
-    if (tecla == 9) { return true; }
-
-    // Patrón de entrada: solo acepta numeros positivos
-    patron = /[0-9]/;
-    tecla_fin = String.fromCharCode(tecla);
-    return patron.test(tecla_fin);
-}
-
-
